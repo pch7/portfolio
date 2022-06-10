4ea82ff609c303ef04587cb3543cbc7ef54f8342
pch7 <75567949+pch7@users.noreply.github.com>
2022-06-10 12:58:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,13 +15,12 @@ const nav = document.querySelector(".nav"),
        const a = navList[i].querySelector("a");
         a.addEventListener("click", function()
         {
-            for(let i=0; i<totalSection; i++){
-                allSection[i].classList.remove("back-section");
-            }
+        removeBackSection();
         for(let j=0; j<totalNavList; j++){
             if(navList[j].querySelector("a").classList.contains("active"))
             {
-                allSection[j].classList.add("back-section");
+                // allSection[j].classList.add("back-section");
+                addBackSection(j)
             }
             navList[j].querySelector("a").classList.remove("active");
         }
@@ -33,6 +32,14 @@ const nav = document.querySelector(".nav"),
         }
         })
     }
+    function removeBackSection(){
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("back-section");
+        }
+    }
+    function addBackSection(num){
+        allSection[num].classList.add("back-section");
+    }
 function showSection(element)
 {
     for(let i=0; i<totalSection; i++){
@@ -41,6 +48,24 @@ function showSection(element)
     const target = element.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active");
 }
+function updateNav(element) { 
+    for(let i=0; i<totalNavList; i++){
+        navList[i].querySelector("a").classList.remove("active"); 
+        const target = element.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+} 
+document.querySelector(".hire-me").addEventListener("click", function() 
+{ 
+    const sectionIndex = this.getAttribute("data-section-index");
+    showSection(this); 
+    updateNav(this); 
+    removeBackSection();
+    addBackSection(sectionIndex);
+})
+    
 const navTogglerBtn = document.querySelector(".nav-toggler"), 
 aside = document.querySelector(".aside"); 
 navTogglerBtn.addEventListener("click", () => 
